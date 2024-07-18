f390a8524051a40466488c1e32ac04657131dba3
RaphaelPortailler <raphael.portailler@lapiscine.pro>
2024-07-18 11:49:16 +0200

    modif affichage article

--- templates/page/showArticleView.php
+++ templates/page/showArticleView.php
@@ -6,6 +6,8 @@
 
         <article>
             <h2><?php echo $article['title']; ?></h2>
+            <p><?php echo $article['content']; ?></p>
+            <p><?php echo $article['date']; ?></p>
         </article>
 
 
